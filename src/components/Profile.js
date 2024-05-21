d9bbf29a469a0b3bd33dfd54e33dd4b87d81579f
Md saud ali <111742012+md-saud@users.noreply.github.com>
2024-05-21 11:52:07 +0530

    Create Profile.js

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <section id="profile" className="profile">
+      <h2>About Me</h2>
+      <p>Hi there! My name is Saud, I am a passionate developer...</p>
+      <img src="%PUBLIC_URL%/path_to_profile_image.jpg" alt="Profile" />
+    </section>
+  );
+}
+
+export default Profile;
