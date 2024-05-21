6379b3f8485cefcbd97ac129a22fe8bf6d7fd3a5
Md saud ali <111742012+md-saud@users.noreply.github.com>
2024-05-21 11:57:53 +0530

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact</h2>
+      <p>You can reach me at: <a href="mailto:saudansari2277@gmail.com">saudansari2277@gmail.com</a></p>
+      <p>You can reach me at: <a href="mailto:saudbs@outlook.com">saudbs@outlook.com</a></p>
+      <p>Follow me on <a href="https://linkedin.com/in/md-saud-ali-635a10244">LinkedIn</a> and <a href="https://twitter.com/saudhaque1">Twitter</a></p>
+    </section>
+  );
+}
+
+export default Contact;
