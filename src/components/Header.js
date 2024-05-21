261ed30a8bafa01fd3f4bf64eb648ebcd37f85f3
Md saud ali <111742012+md-saud@users.noreply.github.com>
2024-05-21 11:47:57 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Saud's Portfolio</h1>
+      <nav>
+        <a href="#profile">Profile</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
